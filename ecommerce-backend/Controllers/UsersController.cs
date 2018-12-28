6016e9f44d25ad65dae2f71dc72d2f8109861663
Aram Koukia <aramkoukia@gmail.com>
2018-12-28 14:06:06 -0800

    added users api

---
+++ ecommerce-backend/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EcommerceApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace EcommerceApi.Controllers
+{
+    [Authorize()]
+    [Produces("application/json")]
+    [Route("api/Users")]
+    public class UsersController : Controller
+    {
+        private readonly EcommerceContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UsersController(
+            EcommerceContext context,
+            UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<IEnumerable<ApplicationUser>> Get()
+        {
+            return await _userManager.Users.ToListAsync();
+        }
+    }
+}
--- ecommerce-backend/Models/ApplicationUser.cs
+++ ecommerce-backend/Models/ApplicationUser.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace EcommerceApi.Models
 {
