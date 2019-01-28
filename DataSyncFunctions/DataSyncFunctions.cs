dd072062dc0916bef375ef038013d204ab4335b9
Aram Koukia <aramkoukia@gmail.com>
2019-01-27 17:49:45 -0800

    fixing sync schedule

--- DataSyncFunctions/DataSyncFunctions.cs
+++ DataSyncFunctions/DataSyncFunctions.cs
@@ -11,17 +11,17 @@ namespace DataSyncFunctions
         static HttpClient client = new HttpClient();
 
         [FunctionName("SyncProducts")]
-        public static async Task Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, TraceWriter log)
+        public static void RunSyncProducts([TimerTrigger("0 0 1 * * *")]TimerInfo myTimer, TraceWriter log)
         {
             log.Info($"SyncProducts: {DateTime.Now}");
-            HttpResponseMessage response = await client.GetAsync("https://lightsandpartsapi.azurewebsites.net/api/sync/products");
+            client.GetAsync("https://lightsandpartsapi.azurewebsites.net/api/sync/products");
         }
 
         [FunctionName("SyncCustomers")]
-        public static async Task RunSyncProducts([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, TraceWriter log)
+        public static void RunSyncCustomers([TimerTrigger("0 0 1 * * *")]TimerInfo myTimer, TraceWriter log)
         {
             log.Info($"Sync Customers: {DateTime.Now}");
-            HttpResponseMessage response = await client.GetAsync("https://lightsandpartsapi.azurewebsites.net/api/sync/customers");
+            client.GetAsync("https://lightsandpartsapi.azurewebsites.net/api/sync/customers");
         }
     }
 }
