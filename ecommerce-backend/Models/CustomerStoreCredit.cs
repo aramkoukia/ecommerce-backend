96022fe9793e9f51f2cdfee562f9e00e216dfa1b
aramkoukia <aramkoukia@gmail.com>
2019-03-17 12:15:15 -0700

    sore credit

--- ecommerce-backend/Models/CustomerStoreCredit.cs
+++ ecommerce-backend/Models/CustomerStoreCredit.cs
@@ -4,17 +4,12 @@ namespace EcommerceApi.Models
 {
     public class CustomerStoreCredit
     {
-        public CustomerStoreCredit()
-        {
-            Customer = new Customer();
-        }
         public int CustomerStoreCreditId { get; set; }
         public int CustomerId { get; set; }
         public string Amount { get; set; }
         public string Notes { get; set; }
         public DateTime CreatedDate { get; set; }
         public string CreatedByUserId { get; set; }
-
         public Customer Customer { get; set; }
     }
 }
