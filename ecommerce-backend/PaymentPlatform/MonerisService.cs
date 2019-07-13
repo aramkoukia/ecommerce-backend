664be1a69e265f019d7db7284eeb605ace46196f
aramkoukia <aramkoukia@gmail.com>
2019-07-13 14:08:27 -0700

    reading config for moneris

--- ecommerce-backend/PaymentPlatform/MonerisService.cs
+++ ecommerce-backend/PaymentPlatform/MonerisService.cs
@@ -3,30 +3,29 @@ using System.Collections.Generic;
 using Microsoft.Net.Http;
 using System.Threading.Tasks;
 using System.Net.Http;
+using Microsoft.Extensions.Configuration;
 
 namespace EcommerceApi.PaymentPlatform
 {
     public class MonerisService
     {
         public HttpClient Client { get; }
+        private readonly IConfiguration _config;
 
-        public MonerisService(HttpClient client)
+        public MonerisService(HttpClient client, IConfiguration config)
         {
-            client.BaseAddress = new Uri("https://api.github.com/");
+            _config = config;
+            client.BaseAddress = new Uri(_config["Moneris:baseUrl"]);
             // GitHub API versioning
-            client.DefaultRequestHeaders.Add("Accept",
-                "application/vnd.github.v3+json");
+            // client.DefaultRequestHeaders.Add("Accept", "application/vnd.github.v3+json");
             // GitHub requires a user-agent
-            client.DefaultRequestHeaders.Add("User-Agent",
-                "HttpClientFactory-Sample");
-
+            // client.DefaultRequestHeaders.Add("User-Agent", "HttpClientFactory-Sample");
             Client = client;
         }
 
         public async Task<IEnumerable<GitHubIssue>> GetAspNetDocsIssues()
         {
-            var response = await Client.GetAsync(
-                "/repos/aspnet/AspNetCore.Docs/issues?state=open&sort=created&direction=desc");
+            var response = await Client.GetAsync("/repos/aspnet/AspNetCore.Docs/issues?state=open&sort=created&direction=desc");
 
             response.EnsureSuccessStatusCode();
 
