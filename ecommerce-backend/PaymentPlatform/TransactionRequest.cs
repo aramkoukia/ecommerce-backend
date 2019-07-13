c691fe319d03f8d089c8f605bd2fa3e8aa579a6d
aramkoukia <aramkoukia@gmail.com>
2019-07-13 15:43:07 -0700

    added request

---
+++ ecommerce-backend/PaymentPlatform/TransactionRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace EcommerceApi.PaymentPlatform
+{
+    public class TransactionRequest
+    {
+        public string StoreId { get; set; }
+        public string ApiToken { get; set; }
+        public string TerminalId { get; set; }
+        public string TxnType { get; set; }
+        public string PostbackUrl { get; set; }
+        public Request Request { get; set; }
+    }
+
+    public class Request
+    {
+        public string OrderId { get; set; }
+        public string Amount { get; set; }
+    }
+}
