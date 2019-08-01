5664d3956835812a5f7ee1a4e856fd9cabe8d9f5
aramkoukia <aramkoukia@gmail.com>
2019-07-31 19:06:09 -0700

    merging controllers

---
+++ ecommerce-backend/Controllers/MonerisController.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using EcommerceApi.Models;
+using EcommerceApi.PaymentPlatform;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceApi.Controllers
+{
+    // [Authorize]
+    [Produces("application/json")]
+    [Route("api/Moneris")]
+    public class MonerisController : Controller
+    {
+
+        [HttpPost("/Pair")]
+        public IActionResult Pair([FromBody] PairRequest pairRequest)
+        {
+            return Ok(pairRequest);
+        }
+
+        [HttpPost("/Initialize")]
+        public IActionResult Initialize([FromBody] InitializeRequest initializeRequest)
+        {
+            return Ok(initializeRequest);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] ValidationResponse validationResponse)
+        {
+            return Ok(validationResponse);
+        }
+    }
+}
