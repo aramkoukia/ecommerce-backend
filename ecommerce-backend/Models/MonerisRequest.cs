418fa116c0da6188095edc10e8b95e9c41c32ae3
aramkoukia <aramkoukia@gmail.com>
2019-08-03 16:43:44 -0700

    more moneris stuff

---
+++ ecommerce-backend/Models/MonerisRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace EcommerceApi.Models
+{
+    public class MonerisRequest
+    {
+
+        public string StoreId { get; set; }
+        public string ApiToken { get; set; }
+        public string TerminalId { get; set; }
+        public string TxnType { get; set; }
+        public string PostbackUrl { get; set; }
+        public Request Request { get; set; }
+    }
+
+    public class Request
+    {
+        public string PairingToken { get; set; }
+    }
+}
