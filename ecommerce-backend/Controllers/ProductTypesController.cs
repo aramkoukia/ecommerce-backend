5b165631d5f9f1dd3e3b6935af6447993908fc98
aramkoukia <aramkoukia@gmail.com>
2019-10-06 20:03:47 -0700

    added product categori api

---
+++ ecommerce-backend/Controllers/ProductTypesController.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EcommerceApi.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace EcommerceApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/ProductTypes")]
+    [Authorize]
+    public class ProductTypesController : Controller
+    {
+        private readonly EcommerceContext _context;
+
+        public ProductTypesController(EcommerceContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProductTypes
+        [HttpGet]
+        public IEnumerable<ProductType> GetProductTypes()
+        {
+            return _context.ProductType;
+        }
+
+        // GET: api/Locations/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductType([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var productType = await _context.ProductType.SingleOrDefaultAsync(m => m.ProductTypeId == id);
+
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(productType);
+        }
+
+        // PUT: api/ProductTypes/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProductType([FromRoute] int id, [FromBody] ProductType productType)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != productType.ProductTypeId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(productType).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductTypeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProductTypes
+        [HttpPost]
+        public async Task<IActionResult> PostProductType([FromBody] ProductType productType)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            productType.ProductTypeId = _context.ProductType.Max(l => l.ProductTypeId) + 1;
+            _context.ProductType.Add(productType);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ProductTypeExists(productType.ProductTypeId))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetProductType", new { id = productType.ProductTypeId }, productType);
+        }
+
+        // DELETE: api/ProductTypes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProductType([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var productType = await _context.ProductType.SingleOrDefaultAsync(m => m.ProductTypeId == id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            _context.ProductType.Remove(productType);
+            await _context.SaveChangesAsync();
+
+            return Ok(productType);
+        }
+
+        private bool ProductTypeExists(int id)
+        {
+            return _context.ProductType.Any(e => e.ProductTypeId == id);
+        }
+    }
+}
