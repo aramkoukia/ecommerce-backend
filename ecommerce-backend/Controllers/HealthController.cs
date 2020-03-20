746f52b5f5987d4f936d51f80a5580849b23865e
Aram Koukia <Aram.Koukia@lgm.ca>
2020-03-20 09:53:05 -0700

    added health api

---
+++ ecommerce-backend/Controllers/HealthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Health")]
+    public class HealthController : Controller
+    {
+        // GET: api/Locations
+        [HttpGet]
+        public string Get() => "Ping";
+    }
+}
