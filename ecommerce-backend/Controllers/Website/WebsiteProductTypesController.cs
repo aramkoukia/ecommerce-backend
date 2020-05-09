3e78d8b2e0b1d62aa3fc9834e116690cf7e331b7
Aram Koukia <aramkoukia@gmail.com>
2020-05-09 10:38:39 -0700

    added website product types

---
+++ ecommerce-backend/Controllers/Website/WebsiteProductTypesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using EcommerceApi.Models;
+
+namespace EcommerceApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Website/ProductTypes")]
+    public class WebsiteProductTypesController : Controller
+    {
+        private readonly EcommerceContext _context;
+
+        public WebsiteProductTypesController(EcommerceContext context) => _context = context;
+
+        // GET: api/ProductTypes
+        [HttpGet]
+        public IEnumerable<ProductType> GetProductTypes() => _context.ProductType;
+
+    }
+}
--- ecommerce-backend/Models/ProductType.cs
+++ ecommerce-backend/Models/ProductType.cs
@@ -7,13 +7,13 @@ namespace EcommerceApi.Models
     {
         public ProductType()
         {
-            // Product = new HashSet<Product>();
         }
 
         public int ProductTypeId { get; set; }
         public string ProductTypeName { get; set; }
         public DateTime ModifiedDate { get; set; }
-
-        // public ICollection<Product> Product { get; set; }
+        public DateTime ThumbnailImagePath { get; set; }
+        public string HeaderImagePath { get; set; }
+        public string Description { get; set; }
     }
 }
