7de5b13d6c40a7ac9d9a2e7468a066b110f3644b
Aram Koukia <aramkoukia@gmail.com>
2020-05-26 11:18:12 -0700

    added post and put apis

--- ecommerce-backend/Controllers/CustomApplicationsController.cs
+++ ecommerce-backend/Controllers/CustomApplicationsController.cs
@@ -43,5 +43,61 @@ namespace EcommerceApi.Controllers
             var stepDetails = await _context.ApplicationStepDetail.Where(m => m.ApplicationStepId == id).ToListAsync();
             return Ok(stepDetails);
         }
+
+        // PUT: api/custom-applications/5/step
+        [HttpPut("{id}/step")]
+        public async Task<IActionResult> PutApplication([FromRoute] int id, [FromBody] ApplicationStep step)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != step.ApplicationStepId)
+            {
+                return BadRequest();
+            }
+
+            var existingStep = await _context.ApplicationStep.FirstOrDefaultAsync(a => a.ApplicationStepId == id);
+            existingStep.StepTitle = step.StepTitle;
+            existingStep.StepDescription = step.StepDescription;
+            existingStep.IsRangeValue = step.IsRangeValue;
+            existingStep.MaxValue = step.MaxValue;
+            existingStep.MinValue = step.MinValue;
+            existingStep.SortOrder = step.SortOrder;
+            existingStep.ValueUnit = step.ValueUnit;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+
+            return Ok(step);
+        }
+
+        // POST: api/custom-applications/5/step
+        [HttpPost("{id}/step")]
+        public async Task<IActionResult> PostApplication([FromBody] ApplicationStep step)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            step.ApplicationStepId = _context.ApplicationStep.Max(l => l.ApplicationStepId) + 1;
+            _context.ApplicationStep.Add(step);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                throw;
+            }
+
+            return CreatedAtAction("GetApplciation", new { id = step.ApplicationStepId }, step);
+        }
     }
 }
