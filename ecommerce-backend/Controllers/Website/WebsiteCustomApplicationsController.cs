9bf795497e8ee57df79843a884e6b2530e352344
Aram Koukia <aramkoukia@gmail.com>
2020-05-28 13:03:03 -0700

    added website custom applications

---
+++ ecommerce-backend/Controllers/Website/WebsiteCustomApplicationsController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using EcommerceApi.Repositories;
+using System.Threading.Tasks;
+using EcommerceApi.ViewModel;
+
+namespace EcommerceApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/website")]
+    public class WebsiteCustomApplicationsController : Controller
+    {
+        private readonly ICustomApplicationRepository _customApplicationRepository;
+
+        public WebsiteCustomApplicationsController(ICustomApplicationRepository customApplicationRepository) =>
+          _customApplicationRepository = customApplicationRepository;
+
+        // GET: api/website/custom-applications
+        [HttpGet("custom-applications")]
+        public async Task<IEnumerable<CustomApplicationViewModel>> Get()
+          => await _customApplicationRepository.GetCustomApplicationSteps();
+    }
+}
