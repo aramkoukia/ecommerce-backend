d8bbe07a0fb2cf00c3f2553f067a79f60400c9db
aramkoukia <aramkoukia@gmail.com>
2020-12-25 13:29:59 -0800

    add some fields

--- ecommerce-backend/ViewModel/Website/WebsiteProductsInCategoryViewModel.cs
+++ ecommerce-backend/ViewModel/Website/WebsiteProductsInCategoryViewModel.cs
@@ -15,9 +15,11 @@ namespace EcommerceApi.ViewModel.Website
         public string ProductTypeName { get; set; }
         public string Balance { get; set; }
         public string Description { get; set; }
+        public string Detail { get; set; }
         public int Rank { get; set; }
         public string SlugsUrl { get; set; }
         public string WarrantyInformation { get; set; }
+        public string AdditionalInformation { get; set; }
         public string UserManualPath { get; set; }
         public string HeaderImagePath { get; set; }
         public List<ProductImage> Images { get; set; }
